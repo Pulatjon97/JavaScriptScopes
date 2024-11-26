2c98a43d16f3b572611d59bfcb0bb7f0d7d690ae
Pulatjon <polatsultonov97@gmail.com>
2024-11-27 07:54:40 +0900

    Hoisting

--- script.js
+++ script.js
@@ -1,21 +1,77 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2024 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2024 - birthYear;
 
-  function printAge() {
-    const output = `${firstName}, You are ${age} years-old, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName}, You are ${age} years-old, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
-    }
-  }
-  printAge();
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       // Creating new variable with same name as outer scope's variable
+//       const firstName = 'Steven';
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-  return age;
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       //Reassigning outer scope's variable
+//       output = 'NEW OUTPUT';
+//     }
+//     // console.log(str);
+//     console.log(millenial);
+//     // console.log(add(2, 3));
+//     console.log(output);
+//   }
+//   printAge();
+
+//   return age;
+// }
+
+// const firstName = 'Ali';
+// calcAge(1997);
+
+//Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Ali';
+let job = 'student';
+const year = 1997;
+
+//Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+function addDecl(a, b) {
+  return a + b;
 }
 
-const firstName = 'Ali';
-calcAge(1997);
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+//Example
+console.log(numProducts);
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
